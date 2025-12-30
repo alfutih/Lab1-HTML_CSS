dca5dea67a25e50123b1c27fe89848550a1134fa
alfutih <alfutih@chalmers.se>
2025-12-31 00:28:22 +0100

    Show validation errors below form inputs

--- contact.js
+++ contact.js
@@ -26,32 +26,6 @@ function validateMessage(message){
     return message.value.trim().length >= 20;
 }
 
-// The problem is when we click submit the browser 
-// sends the form, reloads the page and removes all
-// JS state. To stop the browser from reloading the page so I can validate the 
-// inputs with JS i should use: event.preventDefault();
-
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-    if(!isNotEmpty(firstName)){
-    console.log("The first name is empty");
-    }
-
-    //Implement last name validation function
-
-    if(!isNotEmpty(lastName)){
-        console.log("The last name is empty");
-    }
-
-    //Implement message validation function
-    // The validateMessage function checks if the message length is at least 20 characters and returns true or false. 
-    // If it’s false, I handle the error in the submit logic.”
-
-    if(!validateMessage(message)){
-    console.log("The message must be at least 20 characters.")
-    }
-
-});
 
 //Create function for show error.
 
@@ -77,3 +51,37 @@ function clearError(input){
     //remove red border
     input.classList.remove("input-error");
 }
+
+// The problem is when we click submit the browser 
+// sends the form, reloads the page and removes all
+// JS state. To stop the browser from reloading the page so I can validate the 
+// inputs with JS i should use: event.preventDefault();
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    if(!isNotEmpty(firstName)){
+    showError(firstName, "First name is required");
+    }else{
+        clearError(firstName);
+    }
+
+    //Implement last name validation function
+
+    if(!isNotEmpty(lastName)){
+        showError(lastName, "Last name is required");
+    } else{
+        clearError(lastName);
+    }
+
+    //Implement message validation function
+    // The validateMessage function checks if the message length is at least 20 characters and returns true or false. 
+    // If it’s false, I handle the error in the submit logic.”
+
+    if(!validateMessage(message)){
+    showError(message, "Message must be at least 20 characters");
+    } else {
+        clearError(message);
+    }
+
+});
+
